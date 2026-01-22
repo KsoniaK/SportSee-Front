12f1a46f9e3fd66dcc12c8f9f967baaa66cc9941
sonia kechit <kechits@yahoo.com>
2026-01-22 02:37:58 +0100

    Update frontend build with API URL fix

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
-// config.js
-export const BASE_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/user/"
-    : "https://sportsee-back.onrender.com/user/";
+// // config.js
+// export const BASE_URL =
+//   process.env.NODE_ENV === "development"
+//     ? "http://localhost:3000/user/"
+//     : "https://sportsee-back.onrender.com/user/";
