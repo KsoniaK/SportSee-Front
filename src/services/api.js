d18521db12631a7bbf0a6e1a2aaebd56e8d7980d
sonia kechit <kechits@yahoo.com>
2026-03-07 21:43:53 +0100

    readme

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,12 @@
 // const BASE_URL = "http://localhost:3000/user/";
 import { BASE_URL } from "./config";
 
-export const getUser = async (id) => { // Appel HTTP GET vers : http://localhost:3000/user/12 (ou 18 ici)
-  const response = await fetch(`${BASE_URL}${id}`); // fetch renvoie une promesse / await permet d’attendre la réponse
-  return await response.json(); // conversion de la réponse en JSON / on retourne un objet contenant = non, âge, score et données nutritionnelles
+// Appel HTTP GET vers : http://localhost:3000/user/12 (ou 18 ici)
+export const getUser = async (id) => { 
+  // fetch renvoie une promesse / await permet d’attendre la réponse
+  const response = await fetch(`${BASE_URL}${id}`); 
+  // conversion de la réponse en JSON / on retourne un objet contenant = non, âge, score et données nutritionnelles
+  return await response.json(); 
 };
 
 export const getActivity = async (id) => {
