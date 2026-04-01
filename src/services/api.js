50f68385de1683e7bb2a19f384ef7ea99b621c8f
sonia kechit <kechits@yahoo.com>
2026-04-02 00:42:06 +0200

    api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,25 +2,36 @@
 // const BASE_URL = "http://localhost:3000/user/";
 import { BASE_URL } from "./config";
 
-// Appel HTTP GET vers : http://localhost:3000/user/12 (ou 18 ici)
-export const getUser = async (id) => { 
-  // fetch renvoie une promesse / await permet d’attendre la réponse
-  const response = await fetch(`${BASE_URL}${id}`); 
-  // conversion de la réponse en JSON / on retourne un objet contenant = non, âge, score et données nutritionnelles
-  return await response.json(); 
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.data;
 };
 
-export const getActivity = async (id) => {
-  const response = await fetch(`${BASE_URL}${id}/activity`);
-  return await response.json();
-};
+export const getUser = (id) => fetchData(`${BASE_URL}${id}`);
+export const getActivity = (id) => fetchData(`${BASE_URL}${id}/activity`);
+export const getAverageSessions = (id) => fetchData(`${BASE_URL}${id}/average-sessions`);
+export const getPerformance = (id) => fetchData(`${BASE_URL}${id}/performance`);
 
-export const getAverageSessions = async (id) => {
-  const response = await fetch(`${BASE_URL}${id}/average-sessions`);
-  return await response.json();
-};
+// // Appel HTTP GET vers : http://localhost:3000/user/12 (ou 18 ici)
+// export const getUser = async (id) => { 
+//   // fetch renvoie une promesse / await permet d’attendre la réponse
+//   const response = await fetch(`${BASE_URL}${id}`); 
+//   // conversion de la réponse en JSON / on retourne un objet contenant = non, âge, score et données nutritionnelles
+//   return await response.json(); 
+// };
 
-export const getPerformance = async (id) => {
-  const response = await fetch(`${BASE_URL}${id}/performance`);
-  return await response.json();
-};
+// export const getActivity = async (id) => {
+//   const response = await fetch(`${BASE_URL}${id}/activity`);
+//   return await response.json();
+// };
+
+// export const getAverageSessions = async (id) => {
+//   const response = await fetch(`${BASE_URL}${id}/average-sessions`);
+//   return await response.json();
+// };
+
+// export const getPerformance = async (id) => {
+//   const response = await fetch(`${BASE_URL}${id}/performance`);
+//   return await response.json();
+// };
