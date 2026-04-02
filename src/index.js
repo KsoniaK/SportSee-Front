47cda21cbb9301065454d91a999f9b273bf73ba1
sonia kechit <kechits@yahoo.com>
2026-04-02 13:48:17 +0200

    index.js hashRouter

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 // // Outil de mesure de performances (temps de chargement) = pas necessaire ici
 import reportWebVitals from './reportWebVitals';
 
-// Condition local ou githubPages
-const basename = process.env.NODE_ENV === "production" ? "/SportSee-Front" : ""
+// // Condition local ou githubPages
+// const basename = process.env.NODE_ENV === "production" ? "/SportSee-Front" : ""
 
 // sélectionne la <div id="root"></div> dans public/index.html et crée un root React "moderne"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // il englobe toute l’application. grâce à lui : les routes dynamiques /user/:id , useParams() dans Dashboard et a navigation sans rechargement
-  <Router basename={basename}>
+  <Router>
     <App />
   </Router>
 );
--- src/services/api.js
+++ src/services/api.js
@@ -2,17 +2,6 @@
 // const BASE_URL = "http://localhost:3000/user/";
 import { BASE_URL } from "./config";
 
-// const fetchData = async (url) => {
-//   const response = await fetch(url);
-//   const json = await response.json();
-//   return json.data;
-// };
-
-// export const getUser = (id) => fetchData(`${BASE_URL}${id}`);
-// export const getActivity = (id) => fetchData(`${BASE_URL}${id}/activity`);
-// export const getAverageSessions = (id) => fetchData(`${BASE_URL}${id}/average-sessions`);
-// export const getPerformance = (id) => fetchData(`${BASE_URL}${id}/performance`);
-
 // Appel HTTP GET vers : http://localhost:3000/user/12 (ou 18 ici)
 export const getUser = async (id) => { 
   // fetch renvoie une promesse / await permet d’attendre la réponse
